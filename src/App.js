ca87d4a34563208772406883277a96873e88badd
Evan Prodromou <evangelo@mttr.is>
2021-05-23 11:32:21 -0400

    Add sparklines

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, {useEffect} from 'react';
 import './App.css';
 import NewCasesToday from './NewCasesToday';
 import { useDispatch, useSelector } from 'react-redux';
+import { Sparklines, SparklinesCurve, SparklinesSpots } from 'react-sparklines';
 
 const MONTREAL_URL = "https://data.newcasestoday.app/city/CA/CA_MTR.csv"
 
 function App(props) {
 
   const location = "Montreal";
+
   const newCases = useSelector(state => state.newCases)
   const lastDate = useSelector(state => state.lastDate)
   const delta = useSelector(state => state.delta)
+  const timeline = useSelector(state => state.timeline)
+
   const dispatch = useDispatch();
 
   function getNewCasesToday() {
@@ -23,13 +27,17 @@ function App(props) {
           const data = rows.filter(row => row[0].length > 0)
           const latest = data[data.length - 1]
           const delta = parseInt(latest[1], 10) - parseInt(data[data.length - 2][1], 10)
+          const trend = data.slice(-28 * 2)
+          const timeline = trend.map(datum => datum[1])
+
           if (latest) {
               dispatch({
                   type: "FETCH_NEW_CASES",
                   data: {
                       lastDate: latest[0],
                       newCases: parseInt(latest[1], 10),
-                      delta: delta
+                      delta: delta,
+                      timeline: timeline
                   }
               });
           }
@@ -45,7 +53,11 @@ function App(props) {
     <div className="App">
       <header className="App-header">
         <div className="App-title">New COVID-19 cases in {location} as of <span data-testid="last-date">{lastDate}</span></div>
-        <NewCasesToday newCases={newCases} delta={delta} />
+        <NewCasesToday newCases={newCases} delta={delta} min={0} />
+        <Sparklines data={timeline}>
+          <SparklinesCurve color="red" />
+          <SparklinesSpots />
+        </Sparklines>
       </header>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,15 @@
 let initialState = {
     lastDate: (new Date()).toISOString().slice(0, 10),
     newCases: 0,
-    delta: 0
+    delta: 0,
+    timeline: [0]
 }
 
 export default function reducer(currentState = initialState, action) {
 
     switch (action.type) {
         case "FETCH_NEW_CASES":
-            return { lastDate: action.data.lastDate, newCases: action.data.newCases, delta: action.data.delta }
+            return { lastDate: action.data.lastDate, newCases: action.data.newCases, delta: action.data.delta, timeline: action.data.timeline }
         default:
             return currentState
     }
